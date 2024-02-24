69310c99f019d03cc4ffd884f0403948212bc453
cuongmcph27903 <94911808+cuongmc2407@users.noreply.github.com>
2024-02-24 22:32:58 +0700

    fix

--- readbook_fe/app/src/main/java/com/example/readbook_fe/fragment/ProfileUserDetail.java
+++ readbook_fe/app/src/main/java/com/example/readbook_fe/fragment/ProfileUserDetail.java
@@ -24,7 +24,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
  * create an instance of this fragment.
  */
 public class ProfileUserDetail extends Fragment {
-
     private Toolbar toolbar_profile;
     private TextView tv_fullname_profile, tv_email_profile;
     private User myUser;
@@ -46,7 +45,7 @@ public class ProfileUserDetail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_user, container, false);
+        return inflater.inflate(R.layout.fragment_profile_user_detail, container, false);
     }
 
     @Override
